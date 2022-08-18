8a0678adecabd5af01f84e6a812d068c25ff314d
Bagas Ranggen <bagas.ranggen@thepixelage.com>
2022-08-18 10:40:47 +0700

    Initial commit

---
+++ pages/index.tsx
@@ -0,0 +1,14 @@
+import type { NextPage } from 'next';
+
+const Home: NextPage = () => {
+    return (
+        <div className="container">
+            <h1>Lorem ipsum dolor sit.</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores at dignissimos doloribus dolorum enim facilis illo inventore, laborum odio optio,
+                perferendis possimus
+                quod reiciendis saepe sit soluta sunt temporibus, voluptate.</p>
+        </div>
+    );
+};
+
+export default Home;
