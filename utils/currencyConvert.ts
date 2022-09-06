85f06a22d078bde801e6f41ecf9510defb428229
Bagas Ranggen <bagas.ranggen@gmail.com>
2022-09-06 21:54:36 +0800

    Update currency convert helper

--- utils/currencyConvert.ts
+++ utils/currencyConvert.ts
@@ -1,5 +1,19 @@
-const currencyConvert = (number: number) => {
-    return Math.abs(number).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+const currencyConvert = (number: number, currency: '' | 'Rp' = '', withParentheses?: boolean) => {
+    const currencyResult = Math.abs(number).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',')
+
+    switch (true) {
+        case currency !== '' && withParentheses:
+            return `(${currency}${currencyResult})`;
+
+        case currency !== '':
+            return `${currency}${currencyResult}`;
+
+        case withParentheses:
+            return `(${currencyResult})`;
+
+        default:
+            return currencyResult;
+    }
 };
 
 export default currencyConvert;
