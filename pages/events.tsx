aab6f500b8c5099f7c8bc2420d6852fae6641777
Bagas Ranggen <bagas.ranggen@gmail.com>
2022-09-06 21:53:33 +0800

    Adjust events page

--- pages/events.tsx
+++ pages/events.tsx
@@ -1,28 +1,25 @@
 import React from 'react';
 import type { NextPage } from 'next';
 
-export async function getServerSideProps({ query }: any) {
-    // const month = query?.month ? query.month : 0;
-    // const data = await getExpenseData(1, 'summary');
+import getExpenseData from '../libs/expense';
+
+import DetailPage from '../components/container/detailPage/DetailPage';
+
+export async function getServerSideProps() {
+    const data = await getExpenseData(2, 'detail');
 
     return {
-        // props: { data, month }
-        props: { data: '' }
+        props: { data }
     };
 }
 
 type EventsProps = NextPage & {
     data: Array<any>;
-    month: number;
 };
 
-const Events = ({ data, month }: EventsProps): React.ReactElement => {
-    return (
-        <div className="container">
-            EVENTS
-        </div>
-    );
-};
+const Events = ({ data }: EventsProps): React.ReactElement => (
+    <DetailPage title='Detail by Events' data={data} />
+);
 
 export default Events;
 
