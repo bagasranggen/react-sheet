310a3b3b7cdc3577edab991c175a40380ace51cd
Bagas Ranggen <bagas.ranggen@thepixelage.com>
2022-10-06 16:03:25 +0700

    Check if only accordion header is clicked

--- components/container/detailPage/DetailPage.tsx
+++ components/container/detailPage/DetailPage.tsx
@@ -15,7 +15,13 @@ const DetailPage = ({ title, data }: DetailPageProps): React.ReactElement => {
     const screen = screenResize();
     const [ isOpened, setIsOpened ] = useState<string>('');
 
-    const accordionClickHandler = (key: string) => key === isOpened ? setIsOpened('') : setIsOpened(key);
+    const accordionClickHandler = (e: any, key: string) => {
+        const classList = e.target.parentNode.classList;
+        const check = classList.contains('accordion-header__expense') || classList.contains('accordion-header__wrapper');
+
+        if (!check) return;
+        key === isOpened ? setIsOpened('') : setIsOpened(key);
+    };
 
     return (
         <>
@@ -51,14 +57,14 @@ const DetailPage = ({ title, data }: DetailPageProps): React.ReactElement => {
                                 <Accordion.Item
                                     key={key}
                                     eventKey={key}
-                                    onClick={() => accordionClickHandler(key)}
+                                    onClick={(e: any) => accordionClickHandler(e, key)}
                                     className={isOpened === key ? 'accordion-item--is-focus' : ''}>
                                     <Accordion.Header>
-                                        <div className="row justify-content-between align-items-center gy-1 gy-md-0 w-100">
-                                            <div className="col-md">
-                                                <h4>{data.detail[key][0].title}{data.detail[key][0]?.venue ? ` | ${data.detail[key][0].venue}` : null}{data.detail[key][0]?.eventDate ? ` | ${data.detail[key][0].eventDate}` : null}</h4>
+                                        <div className="row justify-content-between align-items-center gy-1 gy-md-0 w-100 accordion-header__wrapper">
+                                            <div className="col-md accordion-header__title">
+                                                {data.detail[key][0].title}{data.detail[key][0]?.venue ? ` | ${data.detail[key][0].venue}` : null}{data.detail[key][0]?.eventDate ? ` | ${data.detail[key][0].eventDate}` : null}
                                             </div>
-                                            <div className="col-md-auto">
+                                            <div className="col-md-auto accordion-header__expense">
                                                 <span className="text-success">{currencyConvert(summary.income, 'Rp')}</span> | <span className="text-danger">{currencyConvert(summary.expense, 'Rp', true)}</span>
                                             </div>
                                         </div>
